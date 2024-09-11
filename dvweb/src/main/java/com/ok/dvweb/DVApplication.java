443b2365db2babe35558ea24ddfab9c9d24455eb
Victor Bogado <etcdev89@gmail.com>
2024-09-11 19:37:33 -0300

    init proyect

---
+++ dvweb/src/main/java/com/ok/dvweb/DVApplication.java
@@ -0,0 +1,13 @@
+package com.ok.dvweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DVApplication.class, args);
+	}
+
+}
---
+++ dvweb/src/test/java/com/ok/dvweb/DvwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ok.dvweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
